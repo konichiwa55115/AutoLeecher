6105a3059e01a008cc97f7fdcce1baa29d7e315b
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-29 17:14:04 +0200

    Delete conf.py

--- apdbot/conf.py
+++
@@ -1,9 +0,0 @@
-from apdbot.sample_con import Config
-#Fill your all data here Do not Edit other Files
-
-class Config(Config):
-    BOT_TOKEN= "6032076608:AAHGXUrmFaf2_BUtoS5EyGm_SAuOhhfEzAg"
-    APP_ID = int(17983098)
-    API_HASH = "ee28199396e0925f1f44d945ac174f64"
-    OWNER_ID = "1227193881"
-    AUTH = [556315181, -100088888, -10002252254]
